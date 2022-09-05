ed5ccd76fabcfd2af67204a4e21d17c4f44e4db2
GlebAlekseev <glebalekseevjk@gmail.com>
2022-09-05 23:41:15 +0300

    used generic & fixed bug

--- cormen/c10_базовые_структуры/theory/DLinkedList.kt
+++ cormen/c10_базовые_структуры/theory/DLinkedList.kt
@@ -8,139 +8,142 @@ fun main(){
 }
 
 
-class DLinkedList: Iterable<Int>{
-    private var head: Node? = null
-    private var tail: Node? = null
-    var size = 0
+class DLinkedList<T>: Iterable<T>{
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    private var _size = 0
+    val size
+        get() = _size
 
 
-
-    operator fun get(index: Int): Int{
-        if (index == size-1) return tail?.key!!
+    operator fun get(index: Int): T{
+        if (index == _size-1) return tail?.key!!
         val node = findOfIndex(index)
         return node?.key!!
     }
 
     // O(1)
-    fun pushBack(key: Int){
-        if (size == 0){
+    fun pushBack(key: T){
+        if (_size == 0){
             tail = Node(key = key)
             head = tail
         }else{
             tail?.next = Node(key = key, prev = tail)
             tail = tail?.next
         }
-        size++
+        _size++
     }
     // O(1)
-    fun pushFront(key: Int){
-        if (size == 0){
+    fun pushFront(key: T){
+        if (_size == 0){
             tail = Node(key = key)
             head = tail
         }else{
             head?.prev = Node(key = key, next = head)
             head = head?.prev
         }
-        size++
+        _size++
     }
     // O(1)
     fun removeLast(){
-        if (size != 0){
-            if (size == 1){
+        if (_size != 0){
+            if (_size == 1){
                 tail = null
                 head = null
             }else{
                 tail?.prev?.next = null
                 tail = tail?.prev
             }
-            size--
+            _size--
         }else{
             throw RuntimeException("Список уже пуст")
         }
     }
     // O(1)
     fun removeFirst(){
-        if (size != 0){
-            if (size == 1){
+        if (_size != 0){
+            if (_size == 1){
                 tail = null
                 head = null
             }else{
                 head?.next?.prev = null
                 head = head?.next
             }
-            size--
+            _size--
         }else{
             throw RuntimeException("Список уже пуст")
         }
     }
 
     // O(n)
-    private fun findOfIndex(i: Int): Node?{
-        var findedNode: Node? = null
+    private fun findOfIndex(i: Int): Node<T>?{
+        var findedNode: Node<T>? = null
         head?.forEachIndexed{ index, node ->
             if (i == index) findedNode = node
         }
         return findedNode
     }
     // O(n)
-    private fun findOfValue(value: Int): Pair<Node?, Int> {
-        var pair: Pair<Node?, Int> = Pair(null,-1)
+    private fun findOfValue(value: T): Pair<Node<T>?, Int> {
+        var pair: Pair<Node<T>?, Int> = Pair(null,-1)
         head?.forEachIndexed { index, node ->
             if (node.key == value) pair = Pair(node, index)
         }
         return pair
     }
 
-    fun edit(key: Int,index: Int){
+
+    fun edit(key: T,index: Int){
         val node = findOfIndex(index)
         node?.key = key
     }
 
-    fun insert(key: Int,index: Int){
+    fun insert(key: T,index: Int){
         val node = findOfIndex(index)
         val nextNode = node?.next
         val prevNode = node?.prev
         val newNode = node?.copy(key = key)
         nextNode?.prev = newNode
         prevNode?.next = newNode
-        size++
+        _size++
     }
 
-    fun removeOfValue(key: Int){
+
+    fun removeOfValue(key: T){
         val (node, index) = findOfValue(key)
         if (index == 0) removeFirst()
-        else if (index == size) removeLast()
+        else if (index == _size) removeLast()
         else{
             val nextNode = node?.next
             val prevNode = node?.prev
             nextNode?.prev = prevNode
             prevNode?.next = nextNode
         }
-        size--
+        _size--
     }
 
     fun removeOfIndex(index: Int){
         val node = findOfIndex(index)
         if (index == 0) removeFirst()
-        else if (index == size-1) removeLast()
+        else if (index == _size-1) removeLast()
         else{
             val nextNode = node?.next
             val prevNode = node?.prev
             nextNode?.prev = prevNode
             prevNode?.next = nextNode
         }
-        size--
+        _size--
     }
 
     companion object{
-        data class Node(var key: Int,var prev: Node? = null, var next: Node? = null): Iterable<Node>{
-            override fun iterator(): Iterator<Node> = object : Iterator<Node>{
+        data class Node<T>(var key: T,var prev: Node<T>? = null, var next: Node<T>? = null): Iterable<Node<T>>{
+            override fun iterator(): Iterator<Node<T>> = object : Iterator<Node<T>>{
                 var lastNode = this@Node
                 override fun hasNext(): Boolean {
-                    return lastNode.next == null
+                    return lastNode.next != null
                 }
 
-                override fun next(): Node {
+                override fun next(): Node<T> {
                     val tmpNode = lastNode
                     lastNode = lastNode.next!!
                     return tmpNode
@@ -149,13 +152,13 @@ class DLinkedList: Iterable<Int>{
         }
     }
 
-    override fun iterator(): Iterator<Int> = object : Iterator<Int>{
+    override fun iterator(): Iterator<T> = object : Iterator<T>{
         var lastNode = head
         override fun hasNext(): Boolean {
-            return lastNode?.next == null
+            return lastNode != null && lastNode?.next == null
         }
 
-        override fun next(): Int {
+        override fun next(): T {
             val tmpNode = lastNode
             lastNode = lastNode?.next
             return tmpNode?.key!!
