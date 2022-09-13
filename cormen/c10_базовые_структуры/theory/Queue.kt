0c14fcbab4f8472dc4ea6efa5b919a9f5d266d9f
GlebAlekseev <glebalekseevjk@gmail.com>
2022-09-13 22:14:08 +0300

    added generic type

--- cormen/c10_базовые_структуры/theory/Queue.kt
+++ cormen/c10_базовые_структуры/theory/Queue.kt
@@ -1,7 +1,7 @@
 package cormen.c10_базовые_структуры.theory
 
 fun main(){
-    val queue = Queue(5)
+    val queue = Queue<Int>()
     println(queue)
     queue.enqueue(1)
     queue.enqueue(2)
@@ -25,24 +25,32 @@ fun main(){
 
 }
 
-class Queue(val size: Int): Iterable<Int> {
+class Queue<T>: Iterable<T> {
+    private var size = 8
     private var head = 0
     private var tail = 0
     private var queueSize = 0
-    private val array = Array(size){0}
+    private var array = Array<Any?>(size){null} as Array<T?>
 
-    fun enqueue(element: Int){
-        if (isFull()) throw RuntimeException("Overflow")
+    fun enqueue(element: T){
+        if (isFull()) {
+            size *= 2
+            val newArray = Array<Any?>(size){null} as Array<T?>
+            array.forEachIndexed { index, item ->
+                newArray[index] = item
+            }
+            array = newArray
+        }
         queueSize++
         array[tail] = element
         if (tail == size-1) tail = 0 else tail++
     }
-    fun decueue(): Int{
+    fun decueue(): T{
         if (isEmpty()) throw RuntimeException("Underflow")
         queueSize--
         val last = head
         if (head == size-1) head = 0 else head++
-        return array[last]
+        return array[last]!!
     }
     fun isEmpty(): Boolean {
         return queueSize == 0
@@ -51,15 +59,15 @@ class Queue(val size: Int): Iterable<Int> {
         return queueSize == size
     }
 
-    override fun iterator(): Iterator<Int> = object : Iterator<Int> {
+    override fun iterator(): Iterator<T> = object : Iterator<T> {
         var current = 0
         override fun hasNext(): Boolean {
             return current < queueSize
         }
-        override fun next(): Int {
+        override fun next(): T {
             var index = if (head + current >= size) current - (size-head) else head + current
             current++
-            return array[index]
+            return array[index]!!
         }
     }
     override fun toString(): String {
